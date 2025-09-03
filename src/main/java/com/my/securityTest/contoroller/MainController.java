5024e55a11dd4d46b06eb82f0ed4259fdad008ad
ravvitkim <mirkim6086@naver.com>
2025-09-03 11:06:53 +0900

    Security filter chain 설정

---
+++ src/main/java/com/my/securityTest/contoroller/MainController.java
@@ -0,0 +1,18 @@
+package com.my.securityTest.contoroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage() {
+        return "main";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+
+}
